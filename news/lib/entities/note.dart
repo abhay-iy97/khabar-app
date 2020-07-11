c14409d51bb2e9a199b9705dc6e2669af62e2e2d
PAWAN SASANKA AMMANAMANCHI <pass2pawan@gmail.com>
2020-07-11 19:47:57 +0530

    stable app, news list

--- news/lib/entities/note.dart
+++ news/lib/entities/note.dart
@@ -1,15 +1,26 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
 
+// class Note {
+//   String title;
+//   String text;
+//   Image X;
+//   Note(this.title, this.text, this.X);
+
+//   Note.fromJson(Map<String, dynamic> json) {
+//     title = json['title'];
+//     text = json['description'];
+//     X = Image.network(json['articles'][0]['urlToImage']);
+//   }
+// }
 class Note {
   String title;
   String text;
-  Image X;
-  Note(this.title, this.text, this.X);
+
+  Note(this.title, this.text);
 
   Note.fromJson(Map<String, dynamic> json) {
-    title = json['articles'][0]['title'];
-    text = json['articles'][0]['description'];
-    X = Image.network(json['articles'][0]['urlToImage']);
+    title = json['title'];
+    text = json['description'];
   }
 }
--- news/lib/main.dart
+++ news/lib/main.dart
@@ -133,15 +133,18 @@ class _RandomWordsState extends State<RandomWords> {
     //   notes.add(Note.fromJson(news));
     // }
     // return notes;
+    // var url =
+    //     'https://raw.githubusercontent.com/abhay-iy97/kratos/master/news/lib/data.json?token=AEJYMTPMPYLM7KY5KIL6V2C7BG4C6';
+    //
     var url =
-        'https://raw.githubusercontent.com/boriszv/json/master/random_example.json';
+        'https://raw.githubusercontent.com/Shashi456/Deep-Learning/master/data.json';
     var response = await http.get(url);
-
     var notes = List<Note>();
 
     if (response.statusCode == 200) {
       var notesJson = json.decode(response.body);
-      for (var noteJson in notesJson) {
+      //print(notesJson);
+      for (var noteJson in notesJson['articles']) {
         notes.add(Note.fromJson(noteJson));
       }
     }
@@ -155,9 +158,102 @@ class _RandomWordsState extends State<RandomWords> {
     // Now adding the infinite scroll list
     return Scaffold(
       appBar: AppBar(
-        title: Text('Startup Name Generator'),
+        title: Text('News'),
       ),
       body: _buildSuggestions(),
     );
   }
 }
+
+// import 'dart:convert';
+
+// import 'package:flutter/material.dart';
+// import 'package:news/entities/note.dart';
+// import 'package:http/http.dart' as http;
+
+// void main() => runApp(App());
+
+// class App extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: HomePage(),
+//     );
+//   }
+// }
+
+// class HomePage extends StatefulWidget {
+//   @override
+//   _HomePageState createState() => _HomePageState();
+// }
+
+// class _HomePageState extends State<HomePage> {
+//   List<Note> _notes = List<Note>();
+
+//   Future<List<Note>> fetchNotes() async {
+//     // var url =
+//     //     'https://raw.githubusercontent.com/abhay-iy97/kratos/master/news/lib/db.json?token=AEJYMTPMPYLM7KY5KIL6V2C7BG4C6';
+//     // //const String url = 'http://jsonplaceholder.typicode.com/users';
+//     var url =
+//         'https://raw.githubusercontent.com/Shashi456/Deep-Learning/master/data.json';
+//     var response = await http.get(url);
+
+//     var notes = List<Note>();
+
+//     if (response.statusCode == 200) {
+//       var notesJson = json.decode(response.body);
+
+//       for (var noteJson in notesJson['articles']) {
+//         notes.add(Note.fromJson(noteJson));
+//       }
+//     }
+//     return notes;
+//   }
+
+//   @override
+//   void initState() {
+//     fetchNotes().then((value) {
+//       setState(() {
+//         _notes.addAll(value);
+//       });
+//     });
+//     super.initState();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         appBar: AppBar(
+//           title: Text('Apples'),
+//         ),
+//         body: ListView.builder(
+//           itemCount: _notes.length,
+//           itemBuilder: (context, index) {
+//             return Card(
+//               child: Padding(
+//                 padding: const EdgeInsets.only(
+//                     top: 32.0, bottom: 32.0, left: 16.0, right: 16.0),
+//                 child: Column(
+//                   crossAxisAlignment: CrossAxisAlignment.start,
+//                   children: <Widget>[
+//                     Text(
+//                       _notes[index].title,
+//                       style:
+//                           TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+//                     ),
+//                     Text(
+//                       _notes[index].text,
+//                       style: TextStyle(color: Colors.grey.shade600),
+//                     ),
+//                   ],
+//                 ),
+//               ),
+//             );
+//           },
+//         ));
+//   }
+// }
