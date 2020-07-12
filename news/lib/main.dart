697c59fa0597a7fedd72542a58147b0ef8c772f6
AbhishekVashisht <abhishekvashisht007@gmail.com>
2020-07-12 15:16:30 +0530

    TTS

--- news/lib/main.dart
+++ news/lib/main.dart
@@ -204,6 +204,311 @@ class PageDetails extends StatefulWidget {
 }
 
 class _PageDetails extends State<PageDetails> {
+  /*String description =
+      "The Griffith Observatory is the most iconic building in Los Angeles, perched high in the Hollywood Hills, 1,134 feet above sea level.";
+  bool isPlaying = false;
+  FlutterTts _flutterTts;
+
+  @override
+  void initState() {
+    super.initState();
+    initializeTts();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _flutterTts.stop();
+  }
+
+  initializeTts() {
+    _flutterTts = FlutterTts();
+
+    if (PlatformUtil.myPlatform() == MyPlatform.ANDROID) {
+      _flutterTts.ttsInitHandler(() {
+        setTtsLanguage();
+      });
+    } else if (PlatformUtil.myPlatform() == MyPlatform.IOS) {
+      setTtsLanguage();
+    } else if (PlatformUtil.myPlatform() == MyPlatform.WEB) {
+      //not-supported by plugin
+    }
+
+    _flutterTts.setStartHandler(() {
+      setState(() {
+        isPlaying = true;
+      });
+    });
+
+    _flutterTts.setCompletionHandler(() {
+      setState(() {
+        isPlaying = false;
+      });
+    });
+
+    _flutterTts.setErrorHandler((err) {
+      setState(() {
+        print("error occurred: " + err);
+        isPlaying = false;
+      });
+    });
+  }
+
+  void setTtsLanguage() async {
+    await _flutterTts.setLanguage("en-US");
+  }
+
+  void speechSettings1() {
+    _flutterTts.setVoice("en-us-x-sfg#male_1-local");
+    _flutterTts.setPitch(1.5);
+    _flutterTts.setSpeechRate(.9);
+  }
+
+  void speechSettings2() {
+    _flutterTts.setVoice("en-us-x-sfg#male_2-local");
+    _flutterTts.setPitch(1);
+    _flutterTts.setSpeechRate(0.5);
+  }
+
+  Future _speak(String text) async {
+    if (text != null && text.isNotEmpty) {
+      var result = await _flutterTts.speak(text);
+      if (result == 1)
+        setState(() {
+          isPlaying = true;
+        });
+    }
+  }
+
+  Future _stop() async {
+    var result = await _flutterTts.stop();
+    if (result == 1)
+      setState(() {
+        isPlaying = false;
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Container(
+            height: 360,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(50.0),
+                    bottomRight: Radius.circular(50.0)),
+                gradient: LinearGradient(
+                    colors: [Colors.blue, Colors.blueAccent],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight)),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 80),
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Center(
+                    child: Text(
+                      "Griffith Observatory",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28,
+                          fontStyle: FontStyle.normal),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                Expanded(
+                  child: Stack(
+                    children: <Widget>[
+                      Container(
+                        height: double.infinity,
+                        margin: const EdgeInsets.only(
+                            left: 30.0, right: 30.0, top: 10.0),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(30.0),
+                          child: Container(
+                            height: MediaQuery.of(context).size.height / 1.25,
+                            width: MediaQuery.of(context).size.width / 1.25,
+                            child: Image.asset(
+                                "assets/images/griffith_observatory.jpg"),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(description),
+                ),
+                playButton(context),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget playButton(BuildContext context) {
+    return Container(
+      child: Stack(
+        children: <Widget>[
+          Container(
+            padding:
+                const EdgeInsets.symmetric(vertical: 5.0, horizontal: 16.0),
+            margin: const EdgeInsets.only(
+                top: 30, left: 30.0, right: 30.0, bottom: 20.0),
+            child: FlatButton(
+              onPressed: () {
+                //fetch another image
+                setState(() {
+                  //speechSettings1();
+                  isPlaying ? _stop() : _speak(description);
+                });
+              },
+              child: isPlaying
+                  ? Icon(
+                      Icons.stop,
+                      size: 60,
+                      color: Colors.red,
+                    )
+                  : Icon(
+                      Icons.play_arrow,
+                      size: 60,
+                      color: Colors.green,
+                    ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}*/
+  String description =
+      "The Griffith Observatory is the most iconic building in Los Angeles, perched high in the Hollywood Hills, 1,134 feet above sea level.";
+  bool isPlaying = false;
+  FlutterTts _flutterTts;
+
+  @override
+  void initState() {
+    super.initState();
+    initializeTts();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _flutterTts.stop();
+  }
+
+  initializeTts() {
+    _flutterTts = FlutterTts();
+
+    if (PlatformUtil.myPlatform() == MyPlatform.ANDROID) {
+      _flutterTts.ttsInitHandler(() {
+        setTtsLanguage();
+      });
+    } else if (PlatformUtil.myPlatform() == MyPlatform.IOS) {
+      setTtsLanguage();
+    } else if (PlatformUtil.myPlatform() == MyPlatform.WEB) {
+      //not-supported by plugin
+    }
+
+    _flutterTts.setStartHandler(() {
+      setState(() {
+        isPlaying = true;
+      });
+    });
+
+    _flutterTts.setCompletionHandler(() {
+      setState(() {
+        isPlaying = false;
+      });
+    });
+
+    _flutterTts.setErrorHandler((err) {
+      setState(() {
+        print("error occurred: " + err);
+        isPlaying = false;
+      });
+    });
+  }
+
+  void setTtsLanguage() async {
+    await _flutterTts.setLanguage("en-US");
+  }
+
+  void speechSettings1() {
+    _flutterTts.setVoice("en-us-x-sfg#male_1-local");
+    _flutterTts.setPitch(1.5);
+    _flutterTts.setSpeechRate(.9);
+  }
+
+  void speechSettings2() {
+    _flutterTts.setVoice("en-us-x-sfg#male_2-local");
+    _flutterTts.setPitch(1);
+    _flutterTts.setSpeechRate(0.5);
+  }
+
+  Future _speak(String text) async {
+    if (text != null && text.isNotEmpty) {
+      var result = await _flutterTts.speak(text);
+      if (result == 1)
+        setState(() {
+          isPlaying = true;
+        });
+    }
+  }
+
+  Future _stop() async {
+    var result = await _flutterTts.stop();
+    if (result == 1)
+      setState(() {
+        isPlaying = false;
+      });
+  }
+
+  Widget playButton(BuildContext context) {
+    return Container(
+      child: Stack(
+        children: <Widget>[
+          Container(
+            padding:
+                const EdgeInsets.symmetric(vertical: 5.0, horizontal: 16.0),
+            margin: const EdgeInsets.only(
+                top: 30, left: 30.0, right: 30.0, bottom: 20.0),
+            child: FlatButton(
+              onPressed: () {
+                //fetch another image
+                setState(() {
+                  //speechSettings1();
+                  //isPlaying ? _stop() : _speak("descriptions yes no");
+                  _speak("descriptions yes no");
+                });
+              },
+              child: isPlaying
+                  ? Icon(
+                      Icons.stop,
+                      size: 60,
+                      color: Colors.red,
+                    )
+                  : Icon(
+                      Icons.play_arrow,
+                      size: 60,
+                      color: Colors.green,
+                    ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -214,9 +519,10 @@ class _PageDetails extends State<PageDetails> {
         children: [
           Image.network(widget.item.image),
           Text(
-            widget.item.text,
+            "yolo",
             style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
           ),
+          playButton(context),
         ],
       ),
       floatingActionButton: FloatingActionButton(
@@ -321,6 +627,7 @@ class _PageDetails extends State<PageDetails> {
 //         ));
 //   }
 // }
+
 class TTSPluginRecipe extends StatefulWidget {
   @override
   _TTSPluginRecipeState createState() => _TTSPluginRecipeState();
